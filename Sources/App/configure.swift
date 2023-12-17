c35c0205b7b074cec3733e753500b1831bd937d3
ModestSurmach <mosurmach@yandex.ru>
2023-12-17 21:31:05 +0300

    007

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,7 +5,7 @@ import TelegramVaporBot
 public func configure(_ app: Application) async throws {
     
     app.http.server.configuration.hostname = "0.0.0.0"
-    app.http.server.configuration.port = 8080
+    app.http.server.configuration.port = 9090
     
     
 
