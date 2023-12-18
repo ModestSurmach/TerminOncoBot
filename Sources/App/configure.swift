60170f5f49d1218be4656ecc786e0b07fdbcb970
ModestSurmach <mosurmach@yandex.ru>
2023-12-18 21:15:30 +0300

    StartHandlers.001

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -19,7 +19,11 @@ public func configure(_ app: Application) async throws {
     TGBot.log.logLevel = app.logger.logLevel
     let bot: TGBot = .init(app: app, botId: tgApi)
     await TGBOT.setConnection(try await TGLongPollingConnection(bot: bot))
+    
     await DefaultBotHandlers.addHandlers(app: app, connection: TGBOT.connection)
+    await StartHandler.addHandlers(app: app, connection: TGBOT.connection)
+    
+    
     try await TGBOT.connection.start()
 
     try routes(app)
